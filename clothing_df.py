937ec61725108249c785d235f8c11e9b1ffe527e
Kelly Stroh <Kelly@Kellys-MacBook-Pro.local>
2019-07-01 15:32:28 -0700

    again

--- clothing_df.py
+++ clothing_df.py
@@ -16,6 +16,7 @@ idx_series = pd.Series(files, dtype=object).str.replace('/Users/Kelly/galvanize/
 
 pic_arr = np.array(files)
 #### pic_arr = np.array(files)[0:300]
+print(pic_arr)
 
 def get_pixels(files):
     r = np.zeros((len(files),14400))
@@ -60,6 +61,6 @@ wearable_df = df[df['masterCategory'].isin(wearable_list)]
 
 wear_idx = list(wearable_df.index)
 wear_pics = pic_df[pic_df.index.isin(wear_idx)]
-print(wear_pics)
+
 # double check this is True
 # set(wear_pics.index) == set(wearable_df.index)
