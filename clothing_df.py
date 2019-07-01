0db9d160545d6d794310e1f1e2889a9fe56a68da
Kelly Stroh <Kelly@Kellys-MacBook-Pro.local>
2019-07-01 14:32:19 -0700

    try clusters again

--- clothing_df.py
+++ clothing_df.py
@@ -59,3 +59,20 @@ wear_pics = pic_df[pic_df.index.isin(wear_idx)]
 
 # double check this is True
 # set(wear_pics.index) == set(wearable_df.index)
+
+from sklearn.cluster import AgglomerativeClustering
+
+clust = AgglomerativeClustering(n_clusters=3).fit(df.wear_pics)
+
+
+a = df.wear_pics.index
+b = df.wearable_df.loc[a,:]
+c = clust.labels_
+
+b['category_group'] = b['masterCategory'].apply(lambda x: 0 if x =='Apparel' else (1 if x=='Accessories' else 2))
+b['cluster_group'] = c
+d = b[['category_group', 'cluster_group']]
+
+d[d['category_group']==0].groupby('cluster_group').count()
+d[d['category_group']==1].groupby('cluster_group').count()
+d[d['category_group']==2].groupby('cluster_group').count()
