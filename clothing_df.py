abdb854f2afe2f571d5d0c98c024999367867f81
Kelly Stroh <Kelly@Kellys-MacBook-Pro.local>
2019-07-01 14:49:36 -0700

    wear_pics fix attempt # 702

--- clothing_df.py
+++ clothing_df.py
@@ -59,20 +59,21 @@ wear_pics = pic_df[pic_df.index.isin(wear_idx)]
 
 # double check this is True
 # set(wear_pics.index) == set(wearable_df.index)
+########
+wear_pics 
 
-from sklearn.cluster import AgglomerativeClustering
+# from sklearn.cluster import AgglomerativeClustering
 
-clust = AgglomerativeClustering(n_clusters=3).fit(wear_pics)
+# clust = AgglomerativeClustering(n_clusters=3).fit(wear_pics)
 
+# a = wear_pics.index
+# b = wearable_df.loc[a,:]
+# c = clust.labels_
 
-a = wear_pics.index
-b = wearable_df.loc[a,:]
-c = clust.labels_
+# b['category_group'] = b['masterCategory'].apply(lambda x: 0 if x =='Apparel' else (1 if x=='Accessories' else 2))
+# b['cluster_group'] = c
+# d = b[['category_group', 'cluster_group']]
 
-b['category_group'] = b['masterCategory'].apply(lambda x: 0 if x =='Apparel' else (1 if x=='Accessories' else 2))
-b['cluster_group'] = c
-d = b[['category_group', 'cluster_group']]
-
-d[d['category_group']==0].groupby('cluster_group').count()
-d[d['category_group']==1].groupby('cluster_group').count()
-d[d['category_group']==2].groupby('cluster_group').count()
+# d[d['category_group']==0].groupby('cluster_group').count()
+# d[d['category_group']==1].groupby('cluster_group').count()
+# d[d['category_group']==2].groupby('cluster_group').count()
