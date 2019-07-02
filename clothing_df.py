b8533801afb08180e945e09380fd775cf6dbf359
Kelly Stroh <Kelly@Kellys-MacBook-Pro.local>
2019-07-02 10:30:14 -0700

    try first full set cluster

--- clothing_df.py
+++ clothing_df.py
@@ -5,17 +5,19 @@ import glob
 
 # import kaggle dataset 
 df = pd.read_csv('/home/ubuntu/Clothing-Clusters/data/styles.csv', error_bad_lines=False)
+## df = pd.read_csv('~/galvanize/week8/data/styles.csv', error_bad_lines=False)
 df.set_index('id', inplace=True)
 
 # get list of file names for images
-path = r'/home/ubuntu/Clothing-Clusters/data/images'
-#### path = r'/Users/Kelly/galvanize/week8/data/images'
+path = r'/home/ubuntu/Clothing-Clusters/data/images' 
+#path# = r'/Users/Kelly/galvanize/week8/data/images'
 
 files = glob.glob(path + "/*.jpg")
 idx_series = pd.Series(files, dtype=object).str.replace('/home/ubuntu/Clothing-Clusters/data/images/', '').str.replace('.jpg', '')
+## idx_series = pd.Series(files, dtype=object).str.replace('/Users/Kelly/galvanize/week8/data/images/', '').str.replace('.jpg', '')
 
 pic_arr = np.array(files)
-#### pic_arr = np.array(files)[0:300]
+## pic_arr = np.array(files)[0:100]
 
 def get_pixels(files):
     r = np.zeros((len(files),14400))
@@ -39,7 +41,7 @@ def problematic_images(files):
 images_arr = get_pixels(pic_arr)
 
 pic_df0 = pd.DataFrame(images_arr, index=idx_series.astype(int), dtype='int')
-#### pic_df0 = pd.DataFrame(images_arr, index=idx_series[0:300].astype(int), dtype='int')
+## pic_df0 = pd.DataFrame(images_arr, index=idx_series[0:100].astype(int), dtype='int')
 pic_df = pic_df0[pic_df0.notnull()]
 
 pic_idx = set(pic_df.index)
@@ -61,7 +63,7 @@ wearable_df = df[df['masterCategory'].isin(wearable_list)]
 wear_idx = list(wearable_df.index)
 wear_pics = pic_df[pic_df.index.isin(wear_idx)]
 
-print(wear_pics.head())
-print(images_arr.shape)
+# print(wear_pics.head())
+# print(images_arr.shape)
 # double check this is True
 # set(wear_pics.index) == set(wearable_df.index)
--- cluster.py
+++ cluster.py
@@ -4,9 +4,10 @@ import pandas as pd
 from sklearn.cluster import AgglomerativeClustering
 import clothing_df as df
 
+## clust = AgglomerativeClustering(n_clusters=3).fit(df.wear_pics[0:100])
 clust = AgglomerativeClustering(n_clusters=3).fit(df.wear_pics)
 
-
+## a = df.wear_pics[0:100].index
 a = df.wear_pics.index
 b = df.wearable_df.loc[a,:]
 c = clust.labels_
@@ -15,7 +16,7 @@ b['category_group'] = b['masterCategory'].apply(lambda x: 0 if x =='Apparel' els
 b['cluster_group'] = c
 d = b[['category_group', 'cluster_group']]
 
-d[d['category_group']==0].groupby('cluster_group').count()
-d[d['category_group']==1].groupby('cluster_group').count()
+d[d['category_group']==0].groupby('cluster_group').count() 
+d[d['category_group']==1].groupby('cluster_group').count() 
 d[d['category_group']==2].groupby('cluster_group').count()
 
