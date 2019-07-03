0b68d1a358c63d7936c3ec0be1dfd63a2785e890
Kelly Stroh <Kelly@Kellys-MacBook-Pro.local>
2019-07-02 21:38:29 -0700

    add cluster5

---
+++ cluster5.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+from full_df import df, all_img, bw_img
+from subset_df import apparel_subset, accessory_subset, shoe_subset, wearable_subset
+
+from sklearn.decomposition import PCA
+
+pca = PCA(n_components=2)
+
+wear_df, wear_img = wearable_subset(df, bw_img)
+wear_pca = pca.fit_transform(wear_img)
+wearable_colors = {'Apparel':'#008B8B','Accessories':'#808080','Footwear':'#8B0000'}
+df['colors'] = df['masterCategory'].apply(lambda x: wearable_colors[x])
+fig5, ax = plt.subplots(figsize=(6,6))
+ax.scatter( *wear_pca.T, s=.25 , color=df['colors'])
+fig5.savefig('fig0.png')
