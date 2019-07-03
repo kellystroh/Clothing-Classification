d8e772d2268f4fa0f31af335b48607fde5a22861
Kelly Stroh <Kelly@Kellys-MacBook-Pro.local>
2019-07-03 08:11:02 -0700

    fix bug wearable

--- wearable.py
+++ wearable.py
@@ -11,7 +11,7 @@ wearable_list = ['Apparel', 'Accessories', 'Footwear']
 wearable_df = df[ df['masterCategory'].isin(wearable_list) ]
 wearable_img = all_img[ wearable_df.index.values ].copy()
 
-bw_wearables = wearble_img.reshape(-1, 80, 60, 3).mean(3).reshape(-1, 80*60)
+bw_wearables = wearable_img.reshape(-1, 80, 60, 3).mean(3).reshape(-1, 80*60)
 
 pca = PCA(n_components=2)
 bw_wearables_pca = pca.fit_transform(bw_wearables)
@@ -23,3 +23,4 @@ wearable_df['colors'] = wearable_df['masterCategory'].apply(lambda x: masterCat_
 
 fig, ax = plt.subplots(figsize=(6,6))
 ax.scatter( *wearables_pca.T, s=.25 , color=wearable_df['colors'])
+fig.figsave('fig6.png')
