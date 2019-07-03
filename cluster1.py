cbdcc0453b9d2110ad00860402ea33b43bda22cf
Kelly Stroh <Kelly@Kellys-MacBook-Pro.local>
2019-07-03 13:12:02 -0700

    change scatter plot colors for improved contrast

--- cluster1.py
+++ cluster1.py
@@ -18,10 +18,10 @@ pca = PCA(n_components=2)
 #apparel_pca = pca.fit_transform(apparel_img[0:10000])
 apparel_pca = pca.fit_transform(BW_apparel_img)
 
-apparel_subset_colors = {'Bottomwear':'red','Topwear':'blue','Dress':'orange', 'Saree':'magenta', 'Loungewear and Nightwear':'green', 'Innerwear':'grey', 'Apparel Set':'green', 'Socks':'green'  }
+apparel_subset_colors = {'Bottomwear':'red','Topwear':'#D3D3D3','Dress':'orange', 'Saree':'purple', 'Loungewear and Nightwear':'green', 'Innerwear':'blue', 'Apparel Set':'green', 'Socks':'green'  }
 BW_apparel_df['colors'] = BW_apparel_df.loc[:,'subCategory'].apply(lambda x: apparel_subset_colors[x])
 
 fig1, ax = plt.subplots(figsize=(6,6))
 #ax[0].scatter( *BW_apparel_pca.T, s=.35 , color=apparel_df['colors'][0:10000])
-ax.scatter( *apparel_pca.T, s=.35 , color=BW_apparel_df['colors'])
+ax.scatter( *apparel_pca.T, s=.35 , alpha=.7, color=BW_apparel_df['colors'])
 fig1.savefig('fig1.png')
