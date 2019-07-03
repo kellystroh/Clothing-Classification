a320c4e516fd86faabcae17c25d05df8ea01e3ca
Kelly Stroh <Kelly@Kellys-MacBook-Pro.local>
2019-07-03 12:50:16 -0700

    new colors cluster0

--- cluster0.py
+++ cluster0.py
@@ -10,9 +10,9 @@ from sklearn.decomposition import PCA
 pca = PCA(n_components=2)
 
 full_pca = pca.fit_transform(bw_img)
-masterCat_colors = {'Apparel':'#008B8B','Accessories':'#808080','Footwear':'#8B0000', 'Personal Care':'magenta', 'Free Items':'magenta', 'Sporting Goods':'magenta', 'Home':'magenta'}
+masterCat_colors = {'Apparel':'#008B8B','Accessories':'orange','Footwear':'#8B0000', 'Personal Care':'#808080', 'Free Items':'#808080', 'Sporting Goods':'#808080', 'Home':'#808080'}
 df['colors'] = df['masterCategory'].apply(lambda x: masterCat_colors[x])
 fig0, ax = plt.subplots(figsize=(6,6))
-ax.scatter( *full_pca.T, s=.25 , color=df['colors'])
+ax.scatter( *full_pca.T, s=.1 , color=df['colors'])
 fig0.savefig('fig0.png')
 
