171a5ad1299b9396ae26f3afa82d4cd82a81f8de
Kelly Stroh <Kelly@Kellys-MacBook-Pro.local>
2019-07-05 17:55:14 -0700

    move figures to folder; new notebook for Agglomerative models; new .py file for agglo models; new function added to subset_df

--- subset_df.py
+++ subset_df.py
@@ -5,6 +5,13 @@ def wearable_subset(df, pic_df):
     wearable_img = pic_df[ wearable_df.index.values ].copy()
     return (wearable_df, wearable_img)
 
+def top2_subset(df, pic_df):
+    list1 = ['Apparel', 'Accessories']
+    top2_df = df[df['masterCategory'].isin(list1)]
+
+    top2_img = pic_df[ top2_df.index.values ].copy()
+    return (top2_df, top2_img)
+
 def apparel_subset(df, pic_df):
     apparel_df = df[df['masterCategory']=='Apparel']
     apparel_img = pic_df[ apparel_df.index.values ].copy()
