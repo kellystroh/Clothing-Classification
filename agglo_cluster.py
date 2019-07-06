0f796a98e41189900c2263accdacf077d19f969c
Kelly Stroh <Kelly@Kellys-MacBook-Pro.local>
2019-07-06 11:40:46 -0700

    add notebooks folder; fix bugs in  agglo .py files; line 38 agglo_cluster; line 28 aggle_two

--- agglo_cluster.py
+++ agglo_cluster.py
@@ -3,34 +3,39 @@ import pandas as pd
 df = pd.read_csv('data/styles.csv', error_bad_lines=False)
 bw_img = np.load('data/bw_array.npy')
 all_img = np.load('data/image_array.npy')
-
+print('data loaded')
 from subset_df import wearable_subset, top2_subset
 
 df_wear, bw_wear_img = wearable_subset(df, bw_img)
 df_wear2, wear_img = wearable_subset(df, all_img)
+print('data subsetted')
 
-df_top, bw_top_img = top2_subset(df, bw_img)
-df_top2, top2_img = top2_subset(df, all_img)
-
-
+df_wear = df_wear.iloc[0:1000, :]
+bw_wear_img = bw_wear_img[0:1000, :]
+df_wear2 = df_wear2.iloc[0:1000, :]
+wear_img = wear_img[0:1000, :]
+print('data truncated')
 from sklearn.cluster import AgglomerativeClustering
 
 ### BW WEARABLE
 three_clust_bw = AgglomerativeClustering(n_clusters=3).fit(bw_wear_img)
+print('agglo model fitted')
+
 df_wear.loc[:,'Cluster_Number'] = three_clust_bw.labels_
 df_wear.loc[:, 'Category_Number'] = df_wear['masterCategory'].apply(lambda x: 0 if x =='Apparel' else (1 if x=='Accessories' else 2))
-
 clust_by_cat1 = []
 for x in range(3):
     w = df_wear[df_wear['Category_Number']==x].groupby('Cluster_Number').count()['masterCategory'].values
     clust_by_cat1.append(w)
+print('yay')
 
 clust3_bw_df = pd.DataFrame(clust_by_cat1, index=['Apparel','Accessories', 'Footwear'], columns=['Cluster 1', 'Cluster 2', 'Cluster 3'])
 clust3_bw_df.to_pickle("agglo3_bw.pkl")
+print('pickle saved')
 
 ### COLOR WEARABLE
 three_clust_color = AgglomerativeClustering(n_clusters=3).fit(wear_img)
-df_wear2.loc[:,'Cluster_Number'] = three_clust.labels_
+df_wear2.loc[:,'Cluster_Number'] = three_clust_color.labels_
 df_wear2.loc[:, 'Category_Number'] = df_wear2['masterCategory'].apply(lambda x: 0 if x =='Apparel' else (1 if x=='Accessories' else 2))
 
 clust_by_cat2 = []
--- agglo_two_cluster.py
+++ agglo_two_cluster.py
@@ -3,12 +3,17 @@ import pandas as pd
 df = pd.read_csv('data/styles.csv', error_bad_lines=False)
 bw_img = np.load('data/bw_array.npy')
 all_img = np.load('data/image_array.npy')
+print('data loaded')
 
 from subset_df import top2_subset
 
 df_top, bw_top_img = top2_subset(df, bw_img)
-df_top2, top2_img = top2_subset(df, all_img)
+#df_top2, top2_img = top2_subset(df, all_img)
+print('data subsetted')
 
+df_top = df_top.iloc[0:3000,:]
+bw_top_img = bw_top_img[0:3000, :]
+print('data truncated')
 
 from sklearn.cluster import AgglomerativeClustering
 
@@ -16,17 +21,18 @@ from sklearn.cluster import AgglomerativeClustering
 two_clust_bw = AgglomerativeClustering(n_clusters=2).fit(bw_top_img)
 df_top.loc[:,'Cluster_Number'] = two_clust_bw.labels_
 df_top.loc[:, 'Category_Number'] = df_top['masterCategory'].apply(lambda x: 0 if x =='Apparel' else 1)
+print('agglo model fitted')
 
 clust_by_cat1 = []
-for x in range(3):
-    w = df_wear[df_wear['Category_Number']==x].groupby('Cluster_Number').count()['masterCategory'].values
+for x in range(2):
+    w = df_top[df_top['Category_Number']==x].groupby('Cluster_Number').count()['masterCategory'].values
     clust_by_cat1.append(w)
 
 clust2_df = pd.DataFrame(clust_by_cat1, index=['Apparel','Accessories'], columns=['Cluster 1', 'Cluster 2'])
 clust2_df.to_pickle("./agglo2_bw.pkl")
-
+print('saved the pickle')
 ### COLOR TOP2
-two_clust_color = AgglomerativeClustering(n_clusters=2).fit(top2_img)
+'''two_clust_color = AgglomerativeClustering(n_clusters=2).fit(top2_img)
 df_top2.loc[:,'Cluster_Number'] = two_clust.labels_
 df_top2.loc[:, 'Category_Number'] = df_wear2['masterCategory'].apply(lambda x: 0 if x =='Apparel' else (1 if x=='Accessories' else 2))
 
@@ -36,4 +42,4 @@ for x in range(3):
     clust_by_cat2.append(w)
 
 clust2_df = pd.DataFrame(clust_by_cat2, index=['Apparel','Accessories'], columns=['Cluster 1', 'Cluster 2'])
-clust2_df.to_pickle("./agglo2_color.pkl")
+clust2_df.to_pickle("./agglo2_color.pkl")'''
