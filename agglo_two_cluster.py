c5611bde76071de24e28fb827789ac6acbe83bf2
Kelly Stroh <Kelly@Kellys-MacBook-Pro.local>
2019-07-06 13:17:22 -0700

    update agglo for test run on AWS; starting with sample size 500

--- agglo_two_cluster.py
+++ agglo_two_cluster.py
@@ -7,13 +7,9 @@ print('data loaded')
 
 from subset_df import top2_subset
 
-df_top, bw_top_img = top2_subset(df, bw_img)
-#df_top2, top2_img = top2_subset(df, all_img)
-print('data subsetted')
-
-df_top = df_top.iloc[0:3000,:]
-bw_top_img = bw_top_img[0:3000, :]
-print('data truncated')
+df_top, bw_top_img = top2_subset(df[0:500], bw_img[0:500])
+df_top2, top2_img = top2_subset(df[0:500], all_img[0:500])
+print('data subsetted & truncated')
 
 from sklearn.cluster import AgglomerativeClustering
 
@@ -31,15 +27,16 @@ for x in range(2):
 clust2_df = pd.DataFrame(clust_by_cat1, index=['Apparel','Accessories'], columns=['Cluster 1', 'Cluster 2'])
 clust2_df.to_pickle("./agglo2_bw.pkl")
 print('saved the pickle')
-### COLOR TOP2
-'''two_clust_color = AgglomerativeClustering(n_clusters=2).fit(top2_img)
+
+
+two_clust_color = AgglomerativeClustering(n_clusters=2).fit(top2_img)
 df_top2.loc[:,'Cluster_Number'] = two_clust.labels_
 df_top2.loc[:, 'Category_Number'] = df_wear2['masterCategory'].apply(lambda x: 0 if x =='Apparel' else (1 if x=='Accessories' else 2))
 
 clust_by_cat2 = []
-for x in range(3):
+for x in range(2):
     w = df_top2[df_top2['Category_Number']==x].groupby('Cluster_Number').count()['masterCategory'].values
     clust_by_cat2.append(w)
 
 clust2_df = pd.DataFrame(clust_by_cat2, index=['Apparel','Accessories'], columns=['Cluster 1', 'Cluster 2'])
-clust2_df.to_pickle("./agglo2_color.pkl")'''
+clust2_df.to_pickle("./agglo2_color.pkl")
