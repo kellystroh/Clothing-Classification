7477aad1c202487787326926deadfcdf4a4a1726
Kelly Stroh <Kelly@Kellys-MacBook-Pro.local>
2019-07-06 16:32:58 -0700

    update agglo files for sample size 10000 and save charts as .npy instead of .pkl

--- agglo_cluster.py
+++ agglo_cluster.py
@@ -5,10 +5,10 @@ from full_df import df, bw_img, all_img
 # bw_img = np.load('data/bw_array.npy')
 # all_img = np.load('data/image_array.npy')
 print('data loaded')
-from subset_df import wearable_subset, top2_subset
+from subset_df import wearable_subset
 
-df_wear, bw_wear_img = wearable_subset(df, bw_img)
-df_wear2, wear_img = wearable_subset(df, all_img)
+df_wear, bw_wear_img = wearable_subset(df.iloc[0:10000,:], bw_img[0:10000,:])
+#df_wear2, wear_img = wearable_subset(df, all_img)
 print('data subsetted')
 
 from sklearn.cluster import AgglomerativeClustering
@@ -19,18 +19,25 @@ print('agglo model fitted')
 
 df_wear.loc[:,'Cluster_Number'] = three_clust_bw.labels_
 df_wear.loc[:, 'Category_Number'] = df_wear['masterCategory'].apply(lambda x: 0 if x =='Apparel' else (1 if x=='Accessories' else 2))
+print('add columns for cluster & category numbers')
+
 clust_by_cat1 = []
 for x in range(3):
     w = df_wear[df_wear['Category_Number']==x].groupby('Cluster_Number').count()['masterCategory'].values
     clust_by_cat1.append(w)
-print('yay')
 
-clust3_bw_df = pd.DataFrame(clust_by_cat1, index=['Apparel','Accessories', 'Footwear'], columns=['Cluster 1', 'Cluster 2', 'Cluster 3'])
-clust3_bw_df.to_pickle("agglo3_bw.pkl")
+
+clust3_df = pd.DataFrame(clust_by_cat1, index=['Apparel','Accessories', 'Footwear'], columns=['Cluster 1', 'Cluster 2', 'Cluster 3'])
+#clust3_bw_df.to_pickle("agglo3_bw.pkl")
+print(clust3_df.columns)
+print(clust3_df.index)
+x = np.array(clust2_df)
+x.save('agglo_clust2')
+
 print('pickle saved')
 
 ### COLOR WEARABLE
-three_clust_color = AgglomerativeClustering(n_clusters=3).fit(wear_img)
+'''three_clust_color = AgglomerativeClustering(n_clusters=3).fit(wear_img)
 df_wear2.loc[:,'Cluster_Number'] = three_clust_color.labels_
 df_wear2.loc[:, 'Category_Number'] = df_wear2['masterCategory'].apply(lambda x: 0 if x =='Apparel' else (1 if x=='Accessories' else 2))
 
@@ -39,5 +46,5 @@ for x in range(3):
     w = df_wear2[df_wear2['Category_Number']==x].groupby('Cluster_Number').count()['masterCategory'].values
     clust_by_cat2.append(w)
 
-clust3_df = pd.DataFrame(clust_by_cat1, index=['Apparel','Accessories', 'Footwear'], columns=['Cluster 1', 'Cluster 2', 'Cluster 3'])
-clust3_df.to_pickle("agglo3_color.pkl")
+clust3_df_col = pd.DataFrame(clust_by_cat1, index=['Apparel','Accessories', 'Footwear'], columns=['Cluster 1', 'Cluster 2', 'Cluster 3'])
+clust3_df_col.to_pickle("agglo3_color.pkl")'''
--- agglo_two_cluster.py
+++ agglo_two_cluster.py
@@ -10,17 +10,18 @@ print('data loaded')
 
 from subset_df import top2_subset
 
-df_top, bw_top_img = top2_subset(df, bw_img)
-df_top2, top2_img = top2_subset(df, all_img)
-print('data subsetted & truncated')
+df_top, bw_top_img = top2_subset(df.iloc[0:10000,:], bw_img[0:10000,:])
+#df_top2, top2_img = top2_subset(df, all_img)
+print('data subsetted ')
 
 from sklearn.cluster import AgglomerativeClustering
 
 ### BW TOP2
 two_clust_bw = AgglomerativeClustering(n_clusters=2).fit(bw_top_img)
+print('agglo model fitted')
 df_top.loc[:,'Cluster_Number'] = two_clust_bw.labels_
 df_top.loc[:, 'Category_Number'] = df_top['masterCategory'].apply(lambda x: 0 if x =='Apparel' else 1)
-print('agglo model fitted')
+print('add columns for cluster & category numbers')
 
 clust_by_cat1 = []
 for x in range(2):
@@ -28,11 +29,15 @@ for x in range(2):
     clust_by_cat1.append(w)
 
 clust2_df = pd.DataFrame(clust_by_cat1, index=['Apparel','Accessories'], columns=['Cluster 1', 'Cluster 2'])
-clust2_df.to_pickle("./agglo2_bw.pkl")
+print(clust2_df.columns)
+print(clust2_df.index)
+x = np.array(clust2_df)
+x.save('agglo_clust2')
+
 print('saved the pickle')
 
-
-two_clust_color = AgglomerativeClustering(n_clusters=2).fit(top2_img)
+### COLOR TOP2
+'''two_clust_color = AgglomerativeClustering(n_clusters=2).fit(top2_img)
 df_top2.loc[:,'Cluster_Number'] = two_clust_color.labels_
 df_top2.loc[:, 'Category_Number'] = df_top2['masterCategory'].apply(lambda x: 0 if x =='Apparel' else (1 if x=='Accessories' else 2))
 
@@ -42,4 +47,4 @@ for x in range(2):
     clust_by_cat2.append(w)
 
 clust2_df = pd.DataFrame(clust_by_cat2, index=['Apparel','Accessories'], columns=['Cluster 1', 'Cluster 2'])
-clust2_df.to_pickle("./agglo2_color.pkl")
+clust2_df.to_pickle("./agglo2_color.pkl")'''
