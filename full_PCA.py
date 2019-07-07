04f0674f3ef43c414e84fabf59e0c9eb4d1c85cc
Kelly Stroh <Kelly@Kellys-MacBook-Pro.local>
2019-07-06 22:11:05 -0700

    add image subplot to Clothing EDA; path changes to full_PCA

--- full_PCA.py
+++ full_PCA.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -12,11 +13,14 @@ from sklearn.decomposition import PCA
 pca = PCA(n_components=2)
 
 full_pca = pca.fit_transform(bw_img)
+fig, ax = plt.subplots(figsize=(6,6))
+ax.scatter(*full_pca.T, s=.1 )
+
 masterCat_colors = {'Apparel':'#FF6347','Accessories':'#48D1CC','Footwear':'#FFA500'}
 # {'Personal Care':'#808080', 'Free Items':'#808080', 'Sporting Goods':'#808080', 'Home':'#808080'}
 df['colors'] = df['masterCategory'].apply(lambda x: masterCat_colors[x])
-fig0, ax = plt.subplots(figsize=(6,6))
+fig, ax = plt.subplots(figsize=(6,6))
 ax.scatter( *full_pca.T, s=.1 , color=df['colors'])
-fig0.savefig('fig0.png')
+fig.savefig('fig0.png')
 
 
