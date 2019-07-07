62d728cf650ca7ecbcef97de5671fb6f0f69d6fa
Kelly Stroh <Kelly@Kellys-MacBook-Pro.local>
2019-07-06 17:45:01 -0700

    rename cluster0 for clarity

--- full_PCA.py
+++ full_PCA.py
@@ -2,15 +2,18 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from full_df import df, all_img, bw_img
-from subset_df import apparel_subset, accessory_subset, shoe_subset, wearable_subset
+from full_df import df, bw_img
+from subset_df import wearable_subset
+
+df, bw_img = wearable_subset(df, bw_img)
 
 from sklearn.decomposition import PCA
 
 pca = PCA(n_components=2)
 
 full_pca = pca.fit_transform(bw_img)
-masterCat_colors = {'Apparel':'#008B8B','Accessories':'orange','Footwear':'#8B0000', 'Personal Care':'#808080', 'Free Items':'#808080', 'Sporting Goods':'#808080', 'Home':'#808080'}
+masterCat_colors = {'Apparel':'#FF6347','Accessories':'#48D1CC','Footwear':'#FFA500'}
+# {'Personal Care':'#808080', 'Free Items':'#808080', 'Sporting Goods':'#808080', 'Home':'#808080'}
 df['colors'] = df['masterCategory'].apply(lambda x: masterCat_colors[x])
 fig0, ax = plt.subplots(figsize=(6,6))
 ax.scatter( *full_pca.T, s=.1 , color=df['colors'])
