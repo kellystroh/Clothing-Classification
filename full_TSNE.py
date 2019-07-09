f4e53afed06e0442369cc3580933c823513f518b
Kelly Stroh <Kelly@Kellys-MacBook-Pro.local>
2019-07-09 12:21:58 -0700

    deleted superfluous code in TSNE + PCA notebooks; added figures with improved formatting for PCA and TSNE subset; test run of tsne file on local computer; edits to run full TSNE on AWS

--- full_TSNE.py
+++ full_TSNE.py
@@ -3,16 +3,31 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-from full_df import df, all_img, bw_img
-from subset_df import apparel_subset, accessory_subset, shoe_subset, wearable_subset
+from full_df import df
+all_img = np.load('data/image_array.npz')['a']
+bw_img = np.load('data/image_array.npz')['b']
+print('data loaded')
 
-
+#from subset_df import apparel_subset, accessory_subset, shoe_subset, wearable_subset
 from sklearn.manifold import TSNE
-
 ### PLOT TSNE ---- ALL
-all_tsne = TSNE(n_components=2, n_iter=1000, perplexity=100).fit_transform(bw_img[0:100])
+for perplex in [30, 50, 75, 100]
+    all_tsne_bw = TSNE(n_components=2, n_iter=1000, perplexity=perplex).fit_transform(bw_img)
+    print('model fitted, perplex =', perplex)
+    np.save('tsne-bw-aws.npy', all_tsne_bw)
+    print('np saved')
+
+all_tsne_col = TSNE(n_components=2, n_iter=1000, perplexity=perplex).fit_transform(bw_img)
+print('model fitted, perplex =', perplex)
+np.save('tsne-bw-aws.npy', all_tsne_col)
+print('np saved')
+
+
+'''
+#generate plot
 colors = {'Apparel':'grey','Accessories':'teal','Footwear':'red', 'Personal Care':'orange', 'Free Items':'yellow', 'Sporting Goods':'black', 'Home':'magenta'}
 df['color'] = df.loc[:,'masterCategory'].apply(lambda x: colors[x])
 fig4, ax = plt.subplots(figsize=(6,6))
 ax.scatter(XX[:,0],XX[:,1], s=.2, color= df['color'])
 fig4.savefig('scatterplot-TSNA.png')
+'''
